9bd9ee47f14980c4ef3bc8c604e118e3d02f5554
Dean Lightning <thedeanlightning@gmail.com>
2021-03-20 04:24:49 +0800

    finish city style

---
+++ src/components/City/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="alphabet">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/style/variable.styl'
+
+.alphabet
+  display flex
+  flex-direction column
+  justify-content center
+  position absolute
+  top 1.58rem
+  right 0
+  bottom 0
+  width 0.4rem
+  .item
+    line-height 0.4rem
+    text-align center
+    color $bgColor
+</style>
