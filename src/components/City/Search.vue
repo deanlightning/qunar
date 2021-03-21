c70dbd40b4e340883bdefefa08fc8257cd19dcc6
Dean Lightning <thedeanlightning@gmail.com>
2021-03-22 00:55:03 +0800

    finish search

--- src/components/City/Search.vue
+++ src/components/City/Search.vue
@@ -1,19 +1,85 @@
 <template>
   <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+    <div class="search-area">
+      <input
+        v-model="keyword"
+        class="search-input"
+        type="text"
+        placeholder="输入城市名或拼音"
+      />
+    </div>
+    <div class="search-content" ref="content" v-show="keyword">
+      <ul>
+        <li
+          class="search-item border-bottom"
+          v-for="item of result"
+          :key="item.id"
+        >
+          {{ item.name }}
+        </li>
+        <li class="search-item border-bottom" v-show="hasNoResult">
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BetterScroll from 'better-scroll'
+
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data() {
+    return {
+      keyword: '',
+      result: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoResult() {
+      return !this.result.length
+    }
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.result = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach(value => {
+            if (
+              value.spell.indexOf(this.keyword) > -1 ||
+              value.name.indexOf(this.keyword) > -1
+            ) {
+              result.push(value)
+            }
+          })
+        }
+        this.result = result
+      }, 100)
+    }
+  },
+  updated() {
+    this.scroll = new BetterScroll(this.$refs.content)
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~@/assets/style/variable.styl'
 
-.search
+.search-area
   height 0.72rem
   padding 0 0.1rem
   background $bgColor
@@ -25,5 +91,19 @@ export default {
     line-height 0.62rem
     text-align center
     border-radius 0.06rem
+    color #666
+.search-content
+  z-index 1
+  overflow hidden
+  position absolute
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  background #eee
+  .search-item
+    line-height 0.62rem
+    padding-left 0.2rem
+    background #fff
     color #666
 </style>
