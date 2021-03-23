34148381c665c178cd67d48ef6444c2109ce00eb
Dean Lightning <thedeanlightning@gmail.com>
2021-03-23 14:43:02 +0800

    add detail list

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,19 +2,44 @@
   <div class="detail">
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :detailList="detailList"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from '../components/Detail/Banner'
 import DetailHeader from '../components/Detail/Header'
+import DetailList from '../components/Detail/List'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      detailList: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人三馆联票',
+              children: [{ title: '成人三馆联票 - 某一连锁店销售' }]
+            },
+            {
+              title: '成人五馆联票'
+            }
+          ]
+        },
+        { title: '学生票' },
+        { title: '儿童票' },
+        { title: '特惠票' }
+      ]
+    }
   }
 }
 </script>
